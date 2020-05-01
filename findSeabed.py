9bf0acabf9e7bea24319a4748b5cf66b25e135d4
adamjeczmien <37955423+adamjeczmien@users.noreply.github.com>
2020-05-01 21:04:02 +0200

    added main

---
+++ findSeabed.py
@@ -0,0 +1,40 @@
+"""READ ME
+
+Sposób działania:
+ - Obcięcie części obrazka (od 2,5m do Dużego menu)
+ - Zamiana koloru na odcienie szarości
+ - Zamknięcie morfologiczne
+ - Progowanie
+ - Otwarcie + zamknięcie
+ - Znalezienie krawędzi - filtr cannyego
+ - Wykluczenie konturów krótszych niż X
+ - Prezentacja
+
+TODO
+todo Usunięcie małych menu (przyciski na obrazie wpływają na wynik)
+todo Wyciągnięcie współrzędnych konturów (Może się przydać do obrazowania głębokości)
+"""
+
+import cv2
+import numpy as np
+
+def findSeabed(frame):
+    frame = frame[0:720, 0:1280]
+    #From 2,5m to bottom menu
+    frame = frame[250:720, 0:1280]
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((10, 10), np.uint8)
+    frameGray = cv2.morphologyEx(frameGray, cv2.MORPH_CLOSE, kernel)
+    thresh, frameGray = cv2.threshold(frameGray, 50, 255, cv2.THRESH_BINARY_INV)
+    frameGray = cv2.morphologyEx(frameGray, cv2.MORPH_OPEN, kernel)
+    frameGray = cv2.morphologyEx(frameGray, cv2.MORPH_CLOSE, kernel)
+    edges = cv2.Canny(frameGray, 50, 50)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contoursToDraw = contours.copy()
+    contoursToDraw.clear()
+    for i in range(len(contours)):
+        if cv2.arcLength(contours[i], False) > 800:
+            contoursToDraw.append(contours[i])
+
+    return contoursToDraw
+
--- main.py
+++ main.py
@@ -4,31 +4,15 @@ Skrypt działa, przy założeniu, że pliki video znajdują się w folderze o na
 w tym samym miejscu co projekt. Jeśli lokalizacja plików video jest inna zmień obiekt 'sourcePath'.
 
 Zamknięcie aktualnego filmu i przejście do następnego -> wciśnij klawisz 'q'
-
-Sposób działania:
- - Obcięcie części obrazka (od 2,5m do Dużego menu)
- - Zamiana koloru na odcienie szarości
- - Zamknięcie morfologiczne
- - Progowanie
- - Otwarcie + zamknięcie
- - Znalezienie krawędzi - filtr cannyego
- - Wykluczenie konturów krótszych niż X
- - Prezentacja
-
-TODO
-todo Usunięcie małych menu (przyciski na obrazie wpływają na wynik)
-todo Wyciągnięcie współrzędnych konturów (Może się przydać do obrazowania głębokości)
 """
 
+from filterFrame import *
+from findSeabed import *
 import cv2
-import numpy as np
-
 videonames = ['video_20190706_2019-07-06_174249','video_20190708_2019-07-08_182109','video_20190708_2019-07-08_182702',
               'video_20190708_2019-07-08_183022','video_20190708_2019-07-08_183118','video_20190708_2019-07-08_183217',
               'video_20190708_2019-07-08_183332','video_20190708_2019-07-08_185441','video_20190708_2019-07-08_185626',
               'video_20190708_2019-07-08_185932','video_20190712_2019-07-12_174940','video_20190712_2019-07-12_175210']
-
-
 
 sourcePath = 'videos\\'
 
@@ -39,32 +23,15 @@ for name in videonames:
     if (cap.isOpened()== False):
       print("Error opening video file"+sourcePath + name +'.mp4')
 
-    kernel = np.ones((10,10),np.uint8)
-
     while (cap.isOpened()):
         # Capture frame-by-frame
         ret, frame = cap.read()
-        
+
         if ret == True:
             framecopy = frame.copy()
-            #Cutting bottom menu from frames
-            framecopy=framecopy[0:720, 0:1280]
-            framecopy = framecopy[250:720,0:1280]
-            frameGray = cv2.cvtColor(framecopy, cv2.COLOR_BGR2GRAY)
-            frameGray = cv2.morphologyEx(frameGray, cv2.MORPH_CLOSE, kernel)
-            thresh, frameGray= cv2.threshold(frameGray,50,255,cv2.THRESH_BINARY_INV)
-            frameGray = cv2.morphologyEx(frameGray, cv2.MORPH_OPEN,kernel)
-            frameGray = cv2.morphologyEx(frameGray, cv2.MORPH_CLOSE, kernel)
-            edges = cv2.Canny(frameGray, 50, 50)
-            contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            contoursToDraw = contours.copy()
-            contoursToDraw.clear()
-            for i in range(len(contours)):
-                if cv2.arcLength(contours[i],False)>800:
-                    contoursToDraw.append(contours[i])
-            edgesColors = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+            framecopy = filterFrame(framecopy)
+            contoursToDraw = findSeabed(framecopy)
             final = frame.copy()
-            final[250:720,0:1280] = framecopy
             cv2.drawContours(final,contoursToDraw,-1,(0,255,255),2,offset=(0,250))
             cv2.imshow('Input', frame)
             cv2.imshow('Final', final)
@@ -77,4 +44,3 @@ for name in videonames:
     cap.release()
     # Closes all the frames
     cv2.destroyAllWindows()
-
